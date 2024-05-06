ac309a95c8dbcf5d8d30327df45fa68a333a2a42
lukasjc120 <143965229+lukasjc120@users.noreply.github.com>
2024-05-06 15:52:36 -0300

    Commit
    Trabajo practico N°1

---
+++ index.jsx
@@ -0,0 +1,45 @@
+const App = () =>  (
+    <div>
+        <h1>¡Hola, mundo!</h1>
+        <p>¡Bienvenidos a React!</p>
+    </div>
+    
+)
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+import React from 'react';
+
+const contactos = [
+  { id: 1, nombre: 'Victoria', apellido: "Perez", telefono: '123-456-7890' },
+  { id: 2, nombre: 'Maria', apellido: "Gomez", telefono: '987-654-3210' },
+  { id: 3, nombre: 'Pedro', apellido: "Garcia", telefono: '545-001-9815' },
+  { id: 4, nombre: 'Julieta', apellido: "Ponce", telefono: '198-010-1910' },
+  { id: 5, nombre: 'Gabriel', apellido: "Fernandez", telefono: '391-088-6510' },
+  { id: 6, nombre: 'Nicolas', apellido: "Guzman", telefono: '416-941-2241' },
+
+ 
+];
+
+const Contacto = ({ nombre, apellido, telefono }) => (
+  <div>
+    <h3>{nombre} {apellido}</h3>
+    <p>Telefono: {telefono}</p>
+  </div>
+);
+
+const Agenda = () => (
+  <div className="contacto">
+    {contactos.map(contacto => (
+      <Contacto
+        key={contacto.id}
+        nombre={contacto.nombre}
+        apellido={contacto.apellido}
+        telefono={contacto.telefono}
+      />
+    ))}
+  </div>
+);
+
+export default Agenda;
